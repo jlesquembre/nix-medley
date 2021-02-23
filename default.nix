f91c6b6fe2974f0c2b8153cad7fd63db032c5db9
José Luis Lafuente <jl@lafuente.me>
2021-02-23 17:48:55 +0100

    Add jdk-with-cert function

--- default.nix
+++ default.nix
@@ -1,8 +1,47 @@
-{ lib ? import <nixpkgs/lib> }:
+{ pkgs ? import <nixpkgs> { } }:
+let
+  lib = pkgs.lib;
+in
 {
   exportVars = (vars:
     lib.concatStringsSep "\n"
       (lib.mapAttrsToList (n: v: ''export ${n}="${v}"'') vars)
     + "\n"
   );
+
+  /**
+    Injects a certificate in the default jdk trust store (the cacerts file)
+
+    Usage:
+
+    my-jdk = jdk-with-cert
+      {
+        jdk = pkgs.jdk15;
+        cert = pkgs.fetchurl {
+          name = "my-cert.pem";
+          url = "https://test.com/ca.pem";
+          sha256 = "1111111111111111111111111111111111111111111111111111";
+        };
+      };
+    NOTE: Instead, you could set the javax.net.ssh.trustStore to a different dir, e.g.:
+    JDK_JAVA_OPTIONS = ''-Djavax.net.ssl.trustStore=${project-dir}/cacerts'';
+  */
+  jdk-with-cert = { jdk ? pkgs.adoptopenjdk-hotspot-bin-15, cert }:
+    let
+      alias = lib.strings.sanitizeDerivationName cert;
+    in
+    jdk.overrideAttrs
+      (oldAttrs: {
+        postFixup =
+          ''
+            ${oldAttrs.postFixup}
+            cacerts=$(find $out -name cacerts -print0)
+            ${jdk}/bin/keytool -importcert -trustcacerts \
+              -storepass changeit \
+              -alias '${alias}' \
+              -keystore $cacerts \
+              -file '${cert}' \
+              -noprompt
+          '';
+      });
 }
